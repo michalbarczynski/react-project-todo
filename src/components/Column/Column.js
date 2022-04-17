3b8125384311a46d8b3fc54b8aca26db74fc7d73
Michal Barczynski <barczynski_sggw@wp.pl>
2022-04-18 01:24:22 +0200

    first empty columns, develope List component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,8 +3,10 @@ import styles from './Column.module.scss';
 const Column = props => {
     return (
         <article className={styles.column}>
-            <span class className={styles.icon + 'fa fa-' + props.icon}></span>
-            <h2 className={styles.title}>{props.title}</h2>
+            <h2 className={styles.title}>
+            <span className={styles.icon + ' fa fa-' + props.icon}></span>
+            {props.title}
+            </h2>
         </article>
     );
 };
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,36 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
+import { useState } from 'react';
 
 const List = () => {
+
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: ??, title: ?? }]);
+    };
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>
                 <h2 className={styles.title}>Things to do <span className={styles.span}>soon</span></h2>
-            </header>  
+            </header>
+
             <p className={styles.description}>Interesting things I want to check out</p>
+
             <section className={styles.columns}>
-                <article><h2>Books</h2></article>
-                <article><h2>Movies</h2></article>
-                <article><h2>Games</h2></article>
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            
+            <form onSubmit={handleSubmit}>
+                <input type="text" />
+                <button>Add column</button>
+            </form>
         </div>
     );
   };
